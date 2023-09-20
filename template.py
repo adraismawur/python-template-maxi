3e2301d42989f433d78cd6ad144c43ca58c34081
Arjan Draisma (wur) <74908173+adraismawur@users.noreply.github.com>
2023-09-20 16:24:26 +0200

    Create template.py

---
+++ template.py
@@ -0,0 +1,13 @@
+"""Main module of your code
+
+Author(s): Arjan Draisma
+
+This code is covered under the GNU General Public License v3.0.
+Please refer to the LICENSE located in the root of this repository.
+"""
+
+import sys
+
+
+if __name__ == "__main__":
+    print(sys.argv[0] + " says: Hello, World!")
